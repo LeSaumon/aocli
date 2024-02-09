ec01c837c445854f7f5a7fc7dfa9fdfe2dc469b9
Your Name <saimon.haze@gmail.com>
2024-02-09 17:10:04 +0100

    2016 1 1

--- puzzles/year2017/day1/first.py
+++ puzzles/year2017/day1/first.py
@@ -1,5 +1,18 @@
+from rich import print as rprint
+from os.path import join
+
 def solve():
-	pass
+	with open(join(__file__[:-8] + "data\input.txt"), "r") as file:
+		data = file.readline().replace("\n", "")
+		result = 0
+		for i, char in enumerate(data):
+			try:
+				next = data[i + 1]
+			except IndexError:
+				next = data[0]
+			if char == next:
+				result += int(char)
+		rprint(result)
 
 if '__name__' == '__main__':
 	solve()
